3943cf2ec2bf638fed03e808e795fe670b872441
Ming Huang <ming.huang@panintelligence.com>
2025-03-19 14:11:50 +0000

    pams - improved filters

--- dockerPublish.sh
+++ dockerPublish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker login
 docker tag mingzilla/pi-api-mcp-server mingzilla/pi-api-mcp-server:latest
-docker tag mingzilla/pi-api-mcp-server mingzilla/pi-api-mcp-server:1.0.8
+docker tag mingzilla/pi-api-mcp-server mingzilla/pi-api-mcp-server:1.0.9
 docker push mingzilla/pi-api-mcp-server:latest
-docker push mingzilla/pi-api-mcp-server:1.0.8
+docker push mingzilla/pi-api-mcp-server:1.0.9
